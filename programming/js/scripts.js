23a276d9ced5dbbecfacef2ff74a53126837e28c
BenGRey5 <bgraynolds@gmail.com>
2023-10-20 11:30:51 -0700

    add to script.js

--- programming/js/scripts.js
+++ programming/js/scripts.js
@@ -1,5 +1,15 @@
-function add{
-    const Q1 = 1
-    const Q
+document.getElementById("Q1")setAttribute("class", "hidden");
+document.getElementById("Q2")setAttribute("class", "hidden");
+document.getElementById("Q3")setAttribute("class", "hidden");
+document.getElementById("Q4")setAttribute("class", "hidden");
+document.getElementById("Q5")setAttribute("class", "hidden");
+document.getElementById("Q6")setAttribute("class", "hidden");
+
+window.onload = function() {
+    document.querySelector("form").onsubmit = function(event) {
+        event.preventDefault
+
+
+    }
+
 }
-this and this
