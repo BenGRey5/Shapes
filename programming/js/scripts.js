586b08e7d0770f14b4d824b682d50e0361750454
BenGRey5 <bgraynolds@gmail.com>
2023-10-20 12:58:02 -0700

    WIP

--- programming/js/scripts.js
+++ programming/js/scripts.js
@@ -1,21 +1,48 @@
-const question1 = document.querySelector(".answer1");
-const question2 = document.querySelector(".answer2");
-const question3 = document.querySelector(".answer3");
-const question4 = document.querySelector(".answer4");
-const question5 = document.querySelector(".answer5");
-const question6 = document.querySelector(".answer6");
+document.addEventListener("DOMContentLoaded", function () {
+    const surveyForm = document.getElementById("radio-form");
 
-const Q1 = document.querySelector("Q1");
-const Q2 = document.querySelector("Q2");
-const Q3 = document.querySelector("Q3");
-const Q4 = document.querySelector("Q4");
-const Q5 = document.querySelector("Q5");
-const Q6 = document.querySelector("Q6");
+    surveyForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the form from submitting
 
-const survey = document.querySelector(".survey")
+        // Get user responses
+        const whyInterested = getSelectedValue('n/a');
+        const codingSkills = getSelectedValue('n/a');
+        const interestedInGames = getSelectedValue('.answer1');
+        const interestInBackendOrDataScience = getSelectedValue('.answer3');
+        const interestInWebDevelopment = getSelectedValue('.answer5');
 
+        // Determine the recommendation
+        let recommendation = "neither";
 
+        if (whyInterested && codingSkills && interestInBackendOrDataScience) {
+            if (interestedInGames === "yes1") {
+                recommendation = "cSharp";
+            } else if (interestInWebDevelopment === "yes3") {
+                recommendation = "javaScript";
+            } else {
+                recommendation = "python";
+            }
+        }
 
+        // Show the recommendation div and hide others
+        hideAllRecommendations();
+        document.getElementById(recommendation).classList.remove("hidden");
+    });
+
+    function getSelectedValue(name) {
+        const radios = document.querySelectorAll(`input[type="radio"][name="${name}"]`);
+        for (const radio of radios) {
+            if (radio.checked) {
+                return radio.value;
+            }
+        }
+        return null;
     }
 
-}
+    function hideAllRecommendations() {
+        const recommendations = document.querySelectorAll(".recommendation");
+        recommendations.forEach((rec) => {
+            rec.classList.add("hidden");
+        });
+    }
+});
