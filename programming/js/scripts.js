a4ff72e2392f275baebc8d66b53c0bc3babf73a6
BenGRey5 <bgraynolds@gmail.com>
2023-10-20 12:30:52 -0700

    taking a break

--- programming/js/scripts.js
+++ programming/js/scripts.js
@@ -5,6 +5,14 @@ const question4 = document.querySelector(".answer4");
 const question5 = document.querySelector(".answer5");
 const question6 = document.querySelector(".answer6");
 
+const Q1 = document.querySelector("Q1");
+const Q2 = document.querySelector("Q2");
+const Q3 = document.querySelector("Q3");
+const Q4 = document.querySelector("Q4");
+const Q5 = document.querySelector("Q5");
+const Q6 = document.querySelector("Q6");
+
+const survey = document.querySelector(".survey")
 
 
 
