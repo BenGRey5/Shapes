d48781f7748e9bb2b52a80d700bbbe1115059f28
BenGRey5 <bgraynolds@gmail.com>
2023-10-27 14:31:08 -0700

    WIP

--- loop-project/js/scripts.js
+++ loop-project/js/scripts.js
@@ -1,37 +1,90 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const end1 = document.getElementById('numberInput');
-    const btn1 = document.getElementById('btn1');
-    const answer1 = document.getElementById('result');
+let inputValue = document.getElementById("numberInput").value;
+console.log("Input Value: " + inputValue);
 
-    function containsNumber(number) {
-        const numAsString = number.toString();
-        return /[123]+/.test(numAsString);
-    }
 
-    function displayNumbers() {
-        let num = parseInt(end1.value);
-        let numbers = "";  
-        let changeArray = [];
 
-    for(let i = 0; i <= num; i++) {
-        if (containsNumber(i)) {
-            if (i.toString().includes('1')) {
-            changeArray.push("BEEP!");
-            } else if (i.toString().includes('2')) {
-            changeArray.push("BOOP!");
-            } else {
-            changeArray.push("Won't you be my neighbor!");
-        }
-        }else {
-            changeArray.push(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     const end1 = document.getElementById('numberInput');
+//     const btn1 = document.getElementById('btn1');
+//     const answer1 = document.getElementById('result');
+
+//     function containsNumber(number) {
+//         const numAsString = number.toString();
+//         return /[123]+/.test(numAsString);
+//     }
+
+//     function displayNumbers() {
+//         let num = parseInt(end1.value);
+//         let numbers = "";  
+//         let changeArray = [];
+
+//     for(let i = 0; i <= num; i++) {
+//         if (containsNumber(i)) {
+//             if (i.toString().includes('1')) {
+//             changeArray.push("BEEP!");
+//             } else if (i.toString().includes('2')) {
+//             changeArray.push("BOOP!");
+//             } else {
+//             changeArray.push("Won't you be my neighbor!");
+//         }
+//         }else {
+//             changeArray.push(i)
         
         
-        }
-        numbers += changeArray[changeArray.length - 1] + " ";
-    }
+//         }
+//         numbers += changeArray[changeArray.length - 1] + " ";
+//     }
     
-    answer1.innerHTML = numbers;
-}
+//     answer1.innerHTML = numbers;
+// }
 
 
 
@@ -53,17 +106,3 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-
-//     let numberInput = document.getElementById("numberInput");
-//     let submit = document.getElementById("submit");
-    
-//     submit.addEventListener("click", function(){
-//         let endOfLine = numberInput.value;
-    
-    
-    
-    
-//     let i
-//     });
-//     });
