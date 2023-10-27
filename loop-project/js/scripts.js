26c4b0f825c01636f4cf2ddf93b9d46940b13a9c
BenGRey5 <bgraynolds@gmail.com>
2023-10-27 10:01:22 -0700

    WIP

--- loop-project/js/scripts.js
+++ loop-project/js/scripts.js
@@ -1,8 +1,13 @@
-let endOfLine = parseInt(document.getElementById("numberInput"));
+document.addEventListener("DOMContentLoaded", function(){
+    let end1 = document.getElementById('numberInput');
+    const btn1 = document.getElementById('btn1');
+    const answer1 = document.getElementById('result')
 
-
-
-
+    function displayNumbers(){
+        answer1.innerHTML =  end1.value;
+    }
+    btn1.addEventListener('click',displayNumbers)
+});
 
 
 
