00abb8ed991bf2b03b0874b1996417fcce9e2ec8
BenGRey5 <bgraynolds@gmail.com>
2023-10-27 13:30:18 -0700

    correct error with finding numbers

--- loop-project/js/scripts.js
+++ loop-project/js/scripts.js
@@ -3,24 +3,37 @@ document.addEventListener("DOMContentLoaded", function(){
     const btn1 = document.getElementById('btn1');
     const answer1 = document.getElementById('result')
 
+    function containsNumber(number) {
+        const numAsString = number.toString();
+        return numAsString.includes('1') || numAsString.includes('2') || numAsString.includes('3');
+    }
+
     function displayNumbers(){
         let num = parseInt(end1.value);
         let numbers = "";  
-
         let changeArray = [];
-        for(let i=0; i<= num; i++) {
-    numbers += i + " ";
-    for(changeArray.push(i)) {
-        if (i==2){
-            2=
+
+    for(let i = 0; i <= num; i++) {
+        if (i==1){
+            changeArray.push("BEEP!");
+        }
+        else if (i==2){
+            changeArray.push("BOOP!");
+        }
+        else if (i==3){
+            changeArray.push("Won't you be my neighbor!");
+        }else {
+            changeArray.push(i)
+        }
+        numbers += i + " ";
         }
     }
-    }
+    
     answer1.innerHTML = numbers;
 }
 
 
-changeArray.push(values);
+
     btn1.addEventListener('click', displayNumbers);
 });
 
