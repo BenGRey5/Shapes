309c6f62da0ce1a64a22b66c1c0fe8f8f61fcb28
BenGRey5 <bgraynolds@gmail.com>
2023-11-03 10:29:20 -0700

    add a switch case

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -1,20 +1,75 @@
+document.addEventListener('DOMCOntentLoaded', function() {
+    const toppingPrices = {
+        Pepperoni: 0.50,
+        Pineapple: 0.25,
+        Onions: .25,
+        Olives: .30,
+        Mushrooms: .25,
+        Canadianbacon: 0.50,
+        Peppers: 0.30,
+        Tomatoes: 0.20,
+    };
+
+
+
 function Pizza(toppings, size){
     this.toppings = toppings;
     this.size = size
 }
 
+Pizza.prototype.calculateToppings = function() {
+let price = 4;
+
+switch (this.toppings) {
+    case "Pepperoni":
+        price += toppingPrices.Pepperoni;
+        break;
+    case "Pineapple":
+        price += toppingPrices.Pineapple;
+        break;
+    case "Onions":
+        price += toppingPrices.Onions;
+        break;
+    case "Olives":
+        price += toppingPrices.Olives;
+        break;
+    case "Mushrooms":
+        price += toppingPrices.Mushrooms;
+        break;
+    case "Canadianbacon":
+        price += toppingPrices.Canadianbacon;
+        break;
+    case "Peppers":
+        price += toppingPrices.Peppers;
+        break;
+    case "Tomatoes":
+        price += toppingPrices.Tomatoes;
+        break;
+    default :
+        price += 0;
+        break;
+}
+
+return price;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Pizza.prototype.calculatePrice = function() {
     return this.size * this.toppings;
 };
 
-const toppingPrices = {
-    Pepperoni: 0.50,
-    Pineapple: 0.25,
-    Onions: .25,
-    Olives: .30,
-    Mushrooms: .25,
-    Canadianbacon: 0.50,
-    Peppers: 0.30,
-    Tomatoes: 0.20,
-    };
 
+
+};
