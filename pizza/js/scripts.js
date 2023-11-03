dd880bd6f692cd520033b8a194a2d7f44cd6021f
BenGRey5 <bgraynolds@gmail.com>
2023-11-03 09:29:24 -0700

    add topping prices object

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -7,3 +7,14 @@ Pizza.prototype.calculatePrice = function() {
     return this.size * this.toppings;
 };
 
+const toppingPrices = {
+    Pepperoni: 0.50,
+    Pineapple: 0.25,
+    Onions: .25,
+    Olives: .30,
+    Mushrooms: .25,
+    Canadianbacon: 0.50,
+    Peppers: 0.30,
+    Tomatoes: 0.20,
+    };
+
